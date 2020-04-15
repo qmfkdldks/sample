801ac0cb761746fb4bced2e8787946dcd56b9121
Brian Kang <qmfkdldks@gmail.com>
2020-04-15 19:14:21 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Home: NextPage = () => (
+  <>
+    <Header>
+      <img
+        src="https://demo.hasthemes.com/arden-preview/arden/assets/img/logo.png"
+        alt="Logo"
+      />
+    </Header>
+    <Title>a simple yet beautiful portfolio homepage</Title>
+    <Tiles></Tiles>
+  </>
+);
+
+export default Home;
+
+export const Header = styled.header`
+  padding-right: 15px;
+  padding-left: 15px;
+`;
+
+export const Title = styled.h2`
+  font-size: calc(1.5625rem + 3.75vw);
+  color: #f2b636;
+  font-weight: 700;
+  font-family: Karla, sans-serif;
+  flex: 0 0 58.333333%;
+  max-width: 58.333333%;
+  padding-right: 15px;
+  padding-left: 15px;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const Tiles = styled.div`
+  max-width: 600px;
+  margin-right: auto;
+  margin-left: auto;
+`;
